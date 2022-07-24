79bbdec6588d497e40e51032cde6fa2d43ade104
matt <matthew.mitchell@posteo.de>
2022-07-24 09:40:55 +0200

    fixed /tmp/path

--- bootstrap.sh
+++ bootstrap.sh
@@ -26,11 +26,11 @@ fi
 
 
 [ ! -d "$HOME/shared" ] && mkdir -p "$HOME/shared"
-sudo cp mount_virtfs.service /etc/systemd/system/
+sudo cp /tmp/mount_virtfs.service /etc/systemd/system/
 sudo systemctl daemon-reload
 
 sudo systemctl enable mount_virtfs.service
 sudo systemctl start mount_virtfs.service
 
 [ ! -d "/etc/X11/xorg.conf.d/" ] && mkdir -p /etc/X11/xorg.conf.d/
-sudo cp 10-monitor.conf /etc/X11/xorg.conf.d/
+sudo cp /tmp/10-monitor.conf /etc/X11/xorg.conf.d/
